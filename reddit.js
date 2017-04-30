abf7fc5be702d27373b6a55ae016445a197544c8
Tony <tonykrub@hotmail.com>
2017-04-30 19:36:58 +0000

    week2 - node workshop

--- reddit.js
+++ reddit.js
@@ -1,25 +1,39 @@
 var request = require('request');
 
+
+function requestJson (url, callback) {
+    request(url, function(err, response) {
+        if (err) {
+            callback(err);
+        }
+        else {
+            try {
+                var parsed = JSON.parse(response.body);
+                callback(null, parsed);
+            }
+            catch(err) {
+                callback(err);
+            }
+        }
+    })
+}
+
+
+
+
 /*
 This function should "return" the default homepage posts as an array of objects
 */
 function getHomepage(callback) {
+    requestJson('https://www.reddit.com/.json', function(err, response) {
+        if (err) {
+            callback(err);  // we dont do console.log('there is an error') because we need to pass 'callback' to the getHomepage function. It is basically a 'contract' meaning if the function needs 'callback' as parameter, we need to callback(err) here. That is why we callback(err) here instead of using console.log('there is an error'). It is the contract! So you have to do it.  Another reason is this function will be used later. We can console.log('there is an error') inside the function that we will use later.
+        }
+        else {
+            callback(null, response.data.children);  // call back with a null becuase there is no error at this point.
+        }
+    })
   // Load reddit.com/.json and call back with the array of posts
-  // TODO: REPLACE request with requestAsJson!
-  request('https://reddit.com/.json', function(err, res) {
-    if (err) {
-      callback(err);
-    }
-    else {
-      try {
-        var response = JSON.parse(res.body);
-        callback(null, response.data.children); // look at the result and explain why we're returning .data.children
-      }
-      catch(e) {
-        callback(e);
-      }
-    }
-  });
 }
 
 /*
@@ -27,6 +41,14 @@ This function should "return" the default homepage posts as an array of objects.
 In contrast to the `getHomepage` function, this one accepts a `sortingMethod` parameter.
 */
 function getSortedHomepage(sortingMethod, callback) {
+    requestJson('https://www.reddit.com/' + sortingMethod + '.json', function(err, response) {
+        if (err) {
+            callback(err);
+        }
+        else {
+            callback(null, response.data.children);
+        }
+    })
   // Load reddit.com/{sortingMethod}.json and call back with the array of posts
   // Check if the sorting method is valid based on the various Reddit sorting methods
 }
@@ -35,6 +57,14 @@ function getSortedHomepage(sortingMethod, callback) {
 This function should "return" the posts on the front page of a subreddit as an array of objects.
 */
 function getSubreddit(subreddit, callback) {
+    requestJson('https://www.reddit.com/r/' + subreddit + '/.json', function(err, response) {
+        if (err) {
+            callback(err);
+        }
+        else {
+            callback(null, response.data.children);
+        }
+    })
   // Load reddit.com/r/{subreddit}.json and call back with the array of posts
 }
 
@@ -43,6 +73,15 @@ This function should "return" the posts on the front page of a subreddit as an a
 In contrast to the `getSubreddit` function, this one accepts a `sortingMethod` parameter.
 */
 function getSortedSubreddit(subreddit, sortingMethod, callback) {
+    requestJson('reddit.com/r/' + subreddit + '/' + sortingMethod + '.json', function(err, response) {
+        if (err) {
+            callback(err);
+        }
+        else {
+            callback(null, response.data.children);
+        }
+    })
+    
   // Load reddit.com/r/{subreddit}/{sortingMethod}.json and call back with the array of posts
   // Check if the sorting method is valid based on the various Reddit sorting methods
 }
@@ -51,10 +90,25 @@ function getSortedSubreddit(subreddit, sortingMethod, callback) {
 This function should "return" all the popular subreddits
 */
 function getSubreddits(callback) {
+    requestJson('https://www.reddit.com/subreddits.json', function(err, response) {
+        if (err) {
+            callback(err);
+        }
+        else {
+           // console.log('response.children', response.data);
+            callback(null, response.data.children);
+        }
+    })
   // Load reddit.com/subreddits.json and call back with an array of subreddits
 }
 
-// Export the API
+
+// export the API
 module.exports = {
-  // ...
+    requestJson: requestJson,
+    getHomepage: getHomepage,
+    getSortedHomepage: getSortedHomepage,
+    getSubreddit: getSubreddit,
+    getSortedSubreddit: getSortedSubreddit,
+    getSubreddits: getSubreddits
 };
